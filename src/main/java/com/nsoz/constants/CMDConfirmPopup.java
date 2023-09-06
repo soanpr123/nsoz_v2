1dd5aa7f8a7b7e836a0291254a197ed1c92a9c1b
Lưu Viết Soạn <44558413+soanpr123@users.noreply.github.com>
2023-09-06 17:05:04 +0700

    add new skill

---
+++ src/main/java/com/nsoz/constants/CMDConfirmPopup.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nsoz.constants;
+
+/**
+ *
+ * @author Admin
+ */
+public class CMDConfirmPopup {
+
+    public static final int CANCEL = 0;
+    public static final int CONFIRM = 1;
+    public static final int NANG_MAT = 2;
+    public static final int NANG_MAT_VIP = 3;
+    public static final int UPGRADE_SOUL_STONE = 5;
+    public static final int NHIEM_VU_DANH_VONG = 4;
+    public static final int NANG_BI_KIP = 100;
+    public static final int NANG_AN_TOC = 6;
+    public static final int CHANGE_USERNAME = 101;
+}
